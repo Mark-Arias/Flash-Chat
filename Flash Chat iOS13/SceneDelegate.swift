042252850e8820ee917d535e38edaeee2128fe21
Mark Arias <mark.arias7@gmail.com>
2023-12-10 21:03:05 -0600

    Removal of extra code, restoration of stock comments.

--- Flash Chat iOS13/SceneDelegate.swift
+++ Flash Chat iOS13/SceneDelegate.swift
@@ -8,20 +8,18 @@
 
 import UIKit
 
-// Testing a change in my local project.
-/// Yet another change that was made
-/// And look at that, more changes.
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
-    // something happened here too
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -29,11 +27,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) { }
-
-    func foo() {
-        // more stuff to commit
-        print("bar")
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
